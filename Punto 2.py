a0cff9aad9f6294575c2568583e24fc912f29275
Mateo Emmanuel Rodriguez <agusssd0@gmail.com>
2021-06-06 20:27:49 -0300

    Create Punto 2.py

---
+++ Punto 2.py
@@ -0,0 +1,21 @@
+def suma_lenta(numero, otro_numero):
+    print("inserte los numeros a sumar")
+numero1 = int(input())
+numero2 = int(input())
+resultado = numero1 + numero2
+for resultado in range(numero1, numero1 + numero2 + 1, 1):
+    resultado = resultado +1
+    resultado = resultado  -1
+    print(resultado)
+print("El resultado es" + str(resultado))
+
+
+    
+
+    
+
+
+
+
+
+
