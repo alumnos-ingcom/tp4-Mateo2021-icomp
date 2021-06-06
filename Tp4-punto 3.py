c908edafcbf32ee075af9a8898852500cf2c4df2
Mateo Emmanuel Rodriguez <agusssd0@gmail.com>
2021-06-06 20:51:39 -0300

    Create Tp4-punto 3.py

---
+++ Tp4-punto 3.py
@@ -0,0 +1,8 @@
+f = float(input("Ingrese grados Fahrenheit"))
+c = float(f - 32) * (5/9)
+print("La conversion de Fahrenheit a Celsius es: ", (c))
+Centigrados = float(input("Ingrese los grados Celsius: "))
+Fahrenheit = float(Centigrados * 9/5) + 32
+print("Los grados Celsius pasados a Fahrenheit son: ", (Fahrenheit))
+  
+  
