93c11e78e14a63dae8c7d9e1c0a3e4f5f282803d
Mateo Emmanuel Rodriguez <agusssd0@gmail.com>
2021-06-06 21:35:54 -0300

    Create Punto 4.py

---
+++ Punto 4.py
@@ -0,0 +1,20 @@
+################
+# Mateo2021-icomp
+# Comparación de números (4)
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+print("Bienvenido")
+print("Ingrese el primer numero")
+n1 = int(input())
+print("Ingrese el segundo numero")
+n2 = int(input())
+
+if n1 < n2 :
+    print("El primero es menor que el segundo",(-1))
+    
+   
+if n1 == n2 :
+    print("Los numeros son iguales",(0))
+    
+if n1 > n2 :
+    print("El primero es mayor que el segundo",(1))
