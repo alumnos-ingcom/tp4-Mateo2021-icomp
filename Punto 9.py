95d863c1690bb12d0cff34b4decf02fd54312c51
Mateo Emmanuel Rodriguez <agusssd0@gmail.com>
2021-06-07 12:37:12 -0300

    Create Punto 9.py

---
+++ Punto 9.py
@@ -0,0 +1,15 @@
+################
+# Mateo2021-icomp
+# Plantilla de ejercicio
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+
+# Números primos
+print("Bienvenido, este programa le dira si un numero es primo")
+numero = int(input("Incerte el numero entero"))
+if numero % 2 == 0:
+    print("True")
+    
+
+
