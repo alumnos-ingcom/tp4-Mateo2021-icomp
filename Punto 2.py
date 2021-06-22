061d29597ace2d8f2ae379cd7b742a5502439070
Mateo Emmanuel Rodriguez <agusssd0@gmail.com>
2021-06-22 00:47:46 -0300

    Update Punto 2.py

--- Punto 2.py
+++ Punto 2.py
@@ -1,19 +1,22 @@
 #################
 #Mateo2021-icomp
-# Suma Lenta (2)
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
-def suma_lenta(numero, otro_numero):
-    print("inserte los numeros a sumar")
-numero1 = int(input())
-numero2 = int(input())
-resultado = numero1 + numero2
-for resultado in range(numero1, numero1 + numero2 + 1, 1):
-    resultado = resultado +1
-    resultado = resultado  -1
-    print(resultado)
-print("El resultado es" + str(resultado))
 
+#Suma Lenta
+
+def prueba():
+    numero= int(input("Ingrese el primer numero: "))
+    otro_numero = int(input("Ingrese el segundo numero: "))
+    resultado = numero + otro_numero
+    for resultado in range(numero, numero + otro_numero + 1, 1):
+        def suma_lenta(numero, otro_numero):
+            resultado = resultado +1
+            resultado = resultado  -1
+        print("El resultado es: " ,resultado)
+
+if __name__ == "__main__":
+    prueba()
 
     
 
