06f08d7cfe003317842483f70c023019c83232a5
Mateo Emmanuel Rodriguez <agusssd0@gmail.com>
2021-06-22 00:47:56 -0300

    Update Punto 5.py

--- Punto 5.py
+++ Punto 5.py
@@ -1,21 +1,21 @@
 ################
 # Mateo2021-icomp
-# Plantilla de ejercicio
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
 
 
 # Numeros Positivos y Negativos
-print("Bienvenido, a continuacion ingrese un valor")
-numero = int(input())
-numerodos = 0
 
-if numero == numerodos :
-    print("El numero es igual a 0")
-    
-if numero > numerodos :
-    print("El numero es positivo")
-    
-if numero < numerodos :
-    print("El numero es negativo")
-    
+def prueba():
+    print("Bienvenido")
+    numero = int(input("Ingrese un numero: "))
+    numerodos = 0
+    if numero == numerodos :
+        print("El numero es igual a 0")
+    if numero > numerodos :
+        print("El numero es positivo")
+    if numero < numerodos :
+        print("El numero es negativo")
+
+if __name__ == "__main__":
+    prueba()
