b2698c304aeaa31d595300973884fb1da8c72e1b
Mateo Emmanuel Rodriguez <agusssd0@gmail.com>
2021-06-22 00:47:59 -0300

    Update Punto 7.py

--- Punto 7.py
+++ Punto 7.py
@@ -1,21 +1,29 @@
 ################
 #Mateo2021-icomp
-# Plantilla de ejercicio
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
 
 
 # Restas sucesivas
-contador = int(0)
 
-dividendo = int(input("Por favor ingrese el dividendo: "))
-divisor = int(input("Por favor ingrese el divisor:" ))
-dividendo = dividendo - divisor
-
-while(dividendo >= 0) :
-    contador = contador + 1;
+def prueba():
+    dividendo = int(input("Por favor ingrese el dividendo: "))
+    divisor = int(input("Por favor ingrese el divisor:" ))
     dividendo = dividendo - divisor
-    resto = contador % 2
-    resto = resto - 1
-    print("el cociente es igual a: " + str(contador))
-    print("el resto es igual a: " + str(resto))
+    def division_lenta(dividendo, divisor):
+       contador = int(0)
+       while(dividendo >= 0) :
+            contador = contador + 1;
+            dividendo = dividendo - divisor
+            resto =  contador % 2
+            resto = resto -1 
+            print("el cociente es igual a: " + str(contador))
+            print("el resto es igual a: " + str(resto))
+            if resto == -1:
+                print("el cociente es igual a: " + str(contador))
+                print("el resto es igual a: " + str(resto +1))
+            pass
+    print(division_lenta(dividendo, divisor))
+
+if __name__ == "__main__":
+    prueba()
