2029caca4407c9f7950837810cf8cc4b01c2c239
Mateo Emmanuel Rodriguez <agusssd0@gmail.com>
2021-06-22 00:47:57 -0300

    Update Punto 6.py

--- Punto 6.py
+++ Punto 6.py
@@ -1,14 +1,23 @@
 ################
 # Mateo2021-icomp
-# Plantilla de ejercicio
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
 
 # Maximo/Minimo
-print("La lista es")
-numeros  = list(range(1,10))
-print(numeros)
-print("El valor minimo es")
-minimo = print(numeros[0])
-print("El valor maximo es")
-maximo = print(numeros[8])
+
+def prueba ():
+    print("La lista es")
+    numeros  = list(range(1,10))
+    print(numeros)
+    def minimo ():
+        print("El valor minimo es")
+        minimo = print(numeros[0])
+        return minimo
+    def maximo ():
+        print("El valor maximo es")
+        maximo = print(numeros[8])
+    print(minimo())
+    print(maximo())
+
+if __name__ == "__main__":
+    prueba()
